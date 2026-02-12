60f6112d8811ab9cd4993a9ef4b937d2d19842ba
Suresh Thapa <sureshthapa6063@gmail.com>
2026-02-12 09:55:51 +0545

    refactor: improve category fetching and formatting in CategoryList component

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -1,22 +1,39 @@
+import { useEffect } from "react"
+
 // dummy data
 const categories = [
-    {id:1, name:"clothes"},
-    {id:2, name:"shoes"},
-    {id:3, name:"electronics"},
-    {id:4, name:"cap"},
-    {id:5, name:"food"}
+  { id: 1, name: "clothes" },
+  { id: 2, name: "shoes" },
+  { id: 3, name: "electronics" },
+  { id: 4, name: "cap" },
+  { id: 5, name: "food" }
 ]
 
 const CategoryList = () => {
+  useEffect(() => {
+    // function to fetch categories from api
+    const getCategories = async () => {
+      try {
+        const response = await fetch(
+          "https://api.escuelajs.co/api/v1/categories"
+        )
+        const data = response.json()
+        console.log(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getCategories()
+  }, [])
+
   return (
     <div className="category-list">
-        {
-            categories.map(cat =>(
-                <div key={cat.id} className="category-name">
-                    {cat.name}
-                </div>
-            ))
-        }
+      {categories.map((cat) => (
+        <div key={cat.id} className="category-name">
+          {cat.name}
+        </div>
+      ))}
     </div>
   )
 }
