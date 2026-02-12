3be7ed048f61a2ef205555ae2ff554dc3a24d654
Suresh Thapa <sureshthapa6063@gmail.com>
2026-02-12 10:37:07 +0545

    feat: add axios for API requests and implement category fetching in CategoryList component style: update Navbar CSS for sticky positioning style: enhance cat-img styling in index.css

--- src/components/CategoryList.jsx
+++ src/components/CategoryList.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react"
 
 const CategoryList = () => {
     const [categories,setCategories] = useState(null)
+    const [isLoading,setIsLoading] = useState(false)
+    const [error,setError] = useState(null)
 
   useEffect(() => {
     // function to fetch categories from api
     const getCategories = async () => {
+        setIsLoading(true)
+        setError('')
       try {
         const response = await fetch(
           "https://api.escuelajs.co/api/v1/categories"
@@ -15,11 +19,23 @@ const CategoryList = () => {
         setCategories(data)
       } catch (error) {
         console.log(error)
+        setError('Error fetching data!')
+      }finally{
+        // set isLoading state false regardless of data is fetched or not
+        setIsLoading(false) 
       }
     }
 
     getCategories()
   }, [])
+
+  if(isLoading){
+    return <p>loading...</p>
+  }
+
+  if(error){
+    return <p>{error}</p>
+  }
 
   return (
     <div className="category-list">
