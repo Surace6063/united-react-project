00af09c00a35ecb527adf29c6085dd83f170fbf9
Suresh Thapa <sureshthapa6063@gmail.com>
2026-02-20 10:10:07 +0545

    feat: add product detail layout and styling with image, info, and add to cart functionality

--- src/pages/ProductDetailPage.jsx
+++ src/pages/ProductDetailPage.jsx
@@ -12,8 +12,35 @@ const ProductDetailPage = () => {
   if(error) return <p>{error}</p>
 
   return (
-    <div>
-      <h1>{product.title}</h1>
+    <div className="container product_detail_section">
+      <img src={product.images[0]} alt={product.title} className="product_image"  />
+
+      {/* product info */}
+      <div className="product_info">
+         <p className="cat_name">
+          {product.category.name}
+         </p>
+         <h2 className="p_title">
+          {product.title}
+         </h2>
+         <p className="p_price">
+          ${product.price}
+         </p>
+
+         <div>
+            <h4 className="desc_title">Description</h4>
+            <p className="p_desc">{product.description}</p>
+         </div>
+
+         {/* add to cart */}
+         <div className="add_to_cart">
+           <button>+</button>
+           <span className="qty">0</span>
+           <button>-</button>
+           <button className="btn-primary">Add to cart</button>
+         </div>
+
+      </div> 
     </div>
   )
 }
